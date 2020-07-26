631457669874066ff9a184be9cb558d37a0ef41a
jp-cordeiro <jpc.araujo@hotmail.com>
2020-07-26 18:08:02 -0300

    components search and list added to Home component

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <footer>
-    <p>Ranek</p>
+    <p>Ranek E-Commerce</p>
   </footer>
 </template>
 
 <script>
 export default {
-  name: "TheHeader",
+  name: "TheHeader"
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,18 @@
 <template>
-  <div></div>
+  <section>
+    <h1>Compre ou Venda</h1>
+  </section>
 </template>
 
 <script>
+import ProductsSearch from '@/components/products/ProductsSearch'
+import ProductsList from '@/components/products/ProductsList'
+
 export default {
-  name: "Home"
+  name: "Home",
+  components:{
+    ProductsSearch,
+    ProductsList
+  }
 };
 </script>
