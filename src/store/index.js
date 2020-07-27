937ed24164480bf92e5bf13235b3e1b7adb5b503
jp-cordeiro <jpc.araujo@hotmail.com>
2020-07-26 21:33:02 -0300

    search and productsList in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,39 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import api from '@/services/services';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    productsList: [],
   },
   mutations: {
+    SET_PRODUCTS_LIST(state, productsList) {
+      state.productsList = productsList;
+    },
   },
   actions: {
+    async loadProducts({ commit }) {
+      const url = '/produto?_limit=10';
+      await api.get(url).then(({ data }) => {
+        const productsList = data;
+        commit('SET_PRODUCTS_LIST', productsList);
+      });
+    },
+    async seacrhByQuery({ commit }, query) {
+      let url = '/produto?_limit=10&q=';
+      await api.get(`${url}${query}`).then(({ data }) => {
+        const productsList = data;
+        console.log(productsList);
+        commit('SET_PRODUCTS_LIST', productsList);
+      });
+    },
   },
-  modules: {
-  }
-})
+  getters: {
+    getProductsList(state) {
+      return state.productsList;
+    },
+  },
+  modules: {},
+});
